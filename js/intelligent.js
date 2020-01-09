add9019a16232462ed7600d171d8330bd7f303c1
roma-bjj <roma_tsakhlo@ukr.net>
2020-01-09 14:14:42 +0200

    added new file intelligent.html

--- js/intelligent.js
+++ js/intelligent.js
@@ -1,4 +1,6 @@
 document.querySelector('.menu-burger-wrapper').onclick = function() {
 	document.querySelector('.menu-burger').classList.toggle('menu-burger-active');
+	document.querySelector('.main-menu').classList.toggle('mobile-nav-active');
 }
+
 
